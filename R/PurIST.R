40a10028d485b2a88f85d90169197a3fd8d8d8f3
Heewon Seo <lootpiz@gmail.com>
2020-05-19 15:43:13 -0400

    R source code udpated

--- R/PurIST.R
+++ R/PurIST.R
@@ -16,10 +16,14 @@
 #' @importFrom gtools inv.logit
 PurIST <- function(dat)
 {
-    biomarkers = c("GPR87", "REG4", "GPR87", "REG4", "KRT6A", 
-        "ANXA10", "BCAR3", "GATA6", "PTGES", "CLDN18", "ITGA3", 
-        "LGALS4", "C16orf74", "DDC", "S100A2", "SLC40A1", "KRT5", 
-        "CLRN3")
+    biomarkers = c("GPR87", "REG4", 
+                   "KRT6A", "ANXA10",
+                   "BCAR3", "GATA6",
+                   "PTGES", "CLDN18",
+                   "ITGA3", "LGALS4",
+                   "C16orf74", "DDC", 
+                   "S100A2", "SLC40A1", 
+                   "KRT5", "CLRN3")
 
     has_genes = is.element(biomarkers, names(dat))
     if (!all(has_genes)) {
@@ -30,7 +34,6 @@ PurIST <- function(dat)
     coefficient <- c(1.994, 2.031, 1.618, 0.922, 1.059, 0.929, 2.505, 0.485)
     intercept <- -6.815
 
-    if (dat["GPR87"] > dat["REG4"])     { penal_1 <- 1 } else { penal_1 <- 0 }
     if (dat["GPR87"] > dat["REG4"])     { penal_1 <- 1 } else { penal_1 <- 0 }
     if (dat["KRT6A"] > dat["ANXA10"])   { penal_2 <- 1 } else { penal_2 <- 0 }
     if (dat["BCAR3"] > dat["GATA6"])    { penal_3 <- 1 } else { penal_3 <- 0 }
